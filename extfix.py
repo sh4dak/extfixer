d73ea5b94e8b8e8dbe09e2dbaad7fb667d7c2204
Igor Ovsyannikov <kamish@outlook.com>
2019-07-27 21:13:20 +0300

    more comments!

--- extfix.py
+++ extfix.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-import os
-import sys
-import magic as fm
-from pathlib import Path
+# я очень люблю, когда всё по полочкам,
+# прям люблю до обсессий. короче я
+# рассортировал импорты в алфавитном порядке
 import argparse
+from pathlib import Path
+import os
 import logging
+
+# и ещё я встречал такое, что импорты из внешних библиотек
+# (не из стандартной библиотеки питона) пишут через пустую строку
+# от системных
+import magic as fm
 
 
 
@@ -34,9 +40,11 @@ class NotInDictionaryException(Exception):
 class ArgumentException(Exception):
     pass
 
+FILENAME_PROHIBITED = ('<', '>', ':', '"', '\\', '|', '?', '*')
+
 def get_ext(file):
     f = file.parts[-1]
-    if any(i in f for i in ('<','>',':','"','\\','|','?','*')):
+    if any(i in f for i in FILENAME_PROHIBITED):
         raise BadFilenameException()
     buf = f.split('.')
     if len(buf)>1:
@@ -49,6 +57,9 @@ def true_ext(file):
     if mime in extsDict:
         return extsDict[mime]
     elif mime in extsBlacklist:
+        # по-хорошему лучше здесь бросать исключение,
+        # и обрабатывать его в check_ext
+        # raise FileInBlacklist()
         return "blacklisted"
     else:
         raise NotInDictionaryException()
@@ -63,10 +74,13 @@ def check_ext(file):
 def change_ext(file):
     extr = true_ext(file)
     ext = get_ext(file)
-    if (ext!='') :
+    # if not ext:
+    if ext != '':
         newpath = Path(str(file)[:-len(ext)]+extr)
     else:
         newpath = Path(str(file)+'.'+extr)
+    # у Path есть свой метод rename(), так проще
+    # file.rename(newpath)
     os.rename(file, newpath)
     return newpath.parts[-1]
 
@@ -74,19 +88,28 @@ def mime_parser(arr):
     files_to_parse = []
     for x in arr:
         for child in x.iterdir():
+            # подсказка от pylint:
+            # Comparison to False should be 'not expr'
             if check_ext(child) == False:
                 files_to_parse.append(child)
     for file in files_to_parse:
         change_ext(file)
-    return 0
+    return 0 # ??? чеэта
 
 def recursive_dirlist_builder(path, arr, count):
+    # вообще, мне не нравятся эти приколы с arr и ps.
+    # TODO как-нибудь причесать это к одному массиву
     ps = []
     for child in path.iterdir():
+        # проверить файл на папку можно и без обращения к magic:
+        # if child.is_dir(): ...
         if fm.detect_from_filename(str(child)).mime_type == 'inode/directory':
-            ps.append(child) 
+            ps.append(child)
     arr.extend(ps)
-    if count==0 or ps==[]:
+    # можно упростить до невозможного,
+    # т.к. bool(0) == False, а bool([]) тоже автоматически ложен
+    # if not count or not ps: ...
+    if count == 0 or ps == []:
         return arr
     else:
         for x in ps:
